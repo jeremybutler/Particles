984f8fc46f828e5a5e31d98067a424f82d0c1817
calebjonasson <caleb.jonasson@gmail.com>
2012-06-26 21:47:10 -0700

    Loading the core and creating the context.

--- js/core/core.js
+++ js/core/core.js
@@ -1,7 +1,19 @@
-core = function()
+/**
+ * Core will be initialized at the very start of runtime.
+ * Class will be accessable throughout the entire application.
+ */
+Core = function()
 {
+	this.context 	= null;
+	this.loaded 	= false;
+	
 	this.appLoader = function()
 	{
 		// Create the base components that the application will run on.
+		this.context = document.getElementById('canvas').getContext('2d');
+		if(this.context) {
+			this.loaded = true;
+		}
+		
 	}
 }
--- js/core/main.js
+++ js/core/main.js
@@ -1,4 +1,15 @@
+var core = null;
+
 // Start of the application setup.
 function onLoad() {
 	// Create a new core.
+	core = new Core();
+	core.appLoader();
+	if(core.loaded) {
+		log("Core has loaded successfully.");
+	}
+}
+
+function log(content) {
+	console.log(content);
 }
