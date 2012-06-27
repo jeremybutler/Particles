4d5cbd36c4487ade43c07046aa4050fec0cedacc
calebjonasson <caleb.jonasson@gmail.com>
2012-06-27 08:26:35 -0700

    Added the base game class.
    Class will handle loops, and main game logic

---
+++ js/Base/Runner.js
@@ -0,0 +1,19 @@
+/**
+ * This class is going to handle the main logic of the game itself.
+ */
+ 
+Runner = function()
+{
+	// Start the game loop
+	this.start = function() {
+		log("Runner Started");
+	}
+	
+	// Pause the game loop
+	this.stop = function() {
+		log("Runner stopped")
+	}
+	
+	
+	
+}
